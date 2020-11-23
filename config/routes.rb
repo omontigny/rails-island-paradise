2c97081b0eb884160aa10839175344d9be414b5a
Olivier Montigny <olivier.wagon498@free.fr>
2020-11-23 16:36:44 +0100

    Init Island Paradise

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # new routes
+
+
+#   namespace :owner do
+#   resources :islands, :comments
+# end
 end
