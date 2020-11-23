ae37c046477d2cce8dd637efe823a80c65550b87
Olivier Montigny <olivier.wagon498@free.fr>
2020-11-23 18:27:25 +0100

    Add Devise and first_name, last_name User

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Associations
+  has_many islands
+  has_many bookings
+  has_many islands through bookings
+
+  # Validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /.*@.*\.([a-z]{2}[a-z]?)/ }
+  validates :password, :confirmation => true,
+                       :length => {:within => 8..40},
+                       :allow_blank => false
+end
