d94d612c1e6127c03b2702da39cfbd44494b97a4
Olivier Montigny <olivier.wagon498@free.fr>
2020-11-25 09:09:48 +0100

    Reorg images - vue index

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,22 +58,58 @@ olivier  = User.new(first_name: 'Olivier', last_name: 'Montigny', email: 'olivie
 file = File.open(Rails.root.join('db/fixtures/avatars/omontigny.jpg'))
 olivier.avatar.attach(io: file, filename: 'omontigny.jpg', content_type: 'image/jpg')
 olivier.save!
-puts "Island #{olivier.first_name} created"
+puts "User #{olivier.first_name} created"
 
 # Islands
-heart = Island.new(name: "Heart Island", description: "Beautiful island in shape of heart in australian coral reef. Perfect for honeymoon.", capacity: 2, ocean: "Pacific ocean", country: "Australia", price_per_day: 200, shark: true)
+# Heart-shaped Island : Fiji (-17.858052,177.20179) Atlantic Ocean
+heart = Island.new(name: "Heart Island", description: "Beautiful island in shape of heart in Fidji. Perfect for honeymoon.", capacity: 2, ocean: "Atlantic Ocean", country: "Fidji", price_per_day: 200, shark: true)
 file = File.open(Rails.root.join('db/fixtures/islands/heart.jpg'))
 heart.picture.attach(io: file, filename: 'heart.jpg', content_type: 'image/jpg')
 heart.owner = coralie
 heart.save!
 puts "Island #{heart.name} created"
 
-cliff = Island.new(name: "Cliff Island", description: "If you like some challenge, the cliff island is for you. It is not easy to access but once you're on top the view is stunning.", capacity: 2, ocean: "Indian ocean", country: "Thailand", price_per_day: 70, shark: false)
-file = File.open(Rails.root.join('db/fixtures/islands/cliff.jpg'))
-cliff.picture.attach(io: file, filename: 'cliff.jpg', content_type: 'image/jpg')
-cliff.owner = coralie
-cliff.save!
-puts "Island #{cliff.name} created"
+
+# Ko Rang Yai Island: Thailand (7.955642,98.446528) (Indian Ocean)
+rangyai = Island.new(name: "Ko Rang Yai Island", description: "Koh Rang Yai is a peaceful island about 5 km off the east coast of Phuket(Thailand), you can enjoy water sports (including sea kayaking) or sunbathe in the afternoons. Adventurous travellers can find pretty good snorkelling near the headland rocks.", capacity: 2, ocean: "Indian ocean", country: "Thailand", price_per_day: 70, shark: false)
+file = File.open(Rails.root.join('db/fixtures/islands/rangyai.jpg'))
+rangyai.picture.attach(io: file, filename: 'rangyai.jpg', content_type: 'image/jpg')
+rangyai.owner = coralie
+rangyai.save!
+puts "Island #{rangyai.name} created"
+
+# whitsunday-islands (-20.1189023,148.9134288)(Australia) (coral Sea)
+whitsunday = Island.new(name: "whitsunday-islands", description: "The Whitsunday Islands are a located off the coast of Queensland, Australia. Known for their beautiful surroundings, Whitehaven beach is one of the most well known beaches in Australia.", capacity: 2, ocean: "Coral Sea", country: "Australia", price_per_day: 70, shark: false)
+file = File.open(Rails.root.join('db/fixtures/islands/whitsunday.jpg'))
+whitsunday.picture.attach(io: file, filename: 'whitsunday.jpg', content_type: 'image/jpg')
+whitsunday.owner = mickael
+whitsunday.save!
+puts "Island #{whitsunday.name} created"
+
+# ShelterIsland (Lake County, Montana, United States of America (47.84144172763476,-114.21063619580997)
+shelterIsland = Island.new(name: "Shelter Island Castle", description: "One of the most expensive homes in Montana perched on its own private island, if you want to leave unforgettable experience, Go there now!", capacity: 2, ocean: "Indian ocean", country: "United States", price_per_day: 70, shark: false)
+file = File.open(Rails.root.join('db/fixtures/islands/shelterIsland.jpg'))
+shelterIsland.picture.attach(io: file, filename: 'shelterIsland.jpg', content_type: 'image/jpg')
+shelterIsland.owner = mickael
+shelterIsland.save!
+puts "Island #{shelterIsland.name} created"
+
+# BlueIsland (Bahamas) (25.04082,-77.37122) (Mer des caraibes)
+blueIsland = Island.new(name: "Blue Island", description: "You need a private jet to reach this incredible site. This fabulous island, home to three miles of treasured coastline and glistening beaches, features elevations up to 100 feet", capacity: 1, ocean: "Carribean Sea", country: "Bahamas", price_per_day: 50, shark: true)
+file = File.open(Rails.root.join('db/fixtures/islands/blueIsland.jpg'))
+blueIsland.picture.attach(io: file, filename: 'blueIsland.jpg', content_type: 'image/jpg')
+blueIsland.owner= coralie
+blueIsland.save!
+puts "Island #{blueIsland.name} created"
+
+#Half Moon Cay, Cat Island, Bahamas (24.5770593, -75.9232759) (Mer des caraibes)
+halfMoon = Island.new(name: "Half Moon Cay", description: "When people think of the Caribbean, they often overlook one of its most gorgeous regions—the Bay Islands of the Southwestern Caribbean. The island is full equiped because also features a dock, a newly constructed helipad, several storerooms, a workshop and a freight elevator", capacity: 1, ocean: "Carribean Sea", country: "Bahamas", price_per_day: 50, shark: true)
+file = File.open(Rails.root.join('db/fixtures/islands/halfMoon.jpg'))
+halfMoon.picture.attach(io: file, filename: 'halfMoon.jpg', content_type: 'image/jpg')
+halfMoon.owner= mickael
+halfMoon.save!
+puts "Island #{halfMoon.name} created"
+
 
 roc = Island.new(name: "The Roc", description: "This island is perfect for people who wants to spend some time alone to better know themselves and discover the meaning of life.", capacity: 1, ocean: "Pacific ocean", country: "Indonesia", price_per_day: 50, shark: true)
 file = File.open(Rails.root.join('db/fixtures/islands/roc.jpg'))
@@ -108,7 +144,7 @@ file = File.open(Rails.root.join('db/fixtures/islands/narval.jpg'))
 narval.picture.attach(io: file, filename: 'narval.jpg', content_type: 'narval/jpg')
 narval.owner = coralie
 narval.save!
-puts "User #{narval.name} created"
+puts "Island #{narval.name} created"
 
 winty = Island.new(name: "Winty", description: "Winty is an world-famous island to admire whales. So take your binoculars and a coat (because it freezing) and come to discover natural beauties.", capacity: 2, ocean: "Groenland sea", country: "Groenland", price_per_day: 150, shark: false)
 file = File.open(Rails.root.join('db/fixtures/islands/winty.jpg'))
