9d8137771c27b8c2dab615b338732759fa01f6fa
Coralie Delhommeau <coraliedelhommeau@gmail.com>
2020-11-25 13:06:06 +0100

    def new, create et index booking

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -22,10 +22,29 @@ class IslandsController < ApplicationController
   #   end
   # end
 
+  # New booking
+  # def new
+  #   set_island
+  #   @booking = Booking.new
+  # end
+
+  def create
+    @booking = Booking.new(booking_params)
+    set_island
+    @booking.island = @island
+    @booking.save
+    redirect_to bookings_path
+  end
+
+
   private
 
   def island_params
     params.require(:island).permit(:name)
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :nb_travelers)
   end
 
   def set_island
