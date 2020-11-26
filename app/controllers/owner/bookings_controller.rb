61430ee80ac6122c3ec7bca4f00cb3d2436c1520
Coralie Delhommeau <coraliedelhommeau@gmail.com>
2020-11-26 17:20:26 +0100

    owner bookings index with possibility of validate or refuse booking

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,6 +1,20 @@
 class Owner::BookingsController < ApplicationController
   # All my bookings
   def index
-    @bookings = current_user.bookings
+    @bookings_pending = current_user.bookings.order(:status, :start_date).where(status: "pending")
+    @bookings_accepted = current_user.bookings.order(:status, :start_date).where(status: "accepted")
+    @bookings_refused = current_user.bookings.order(:status, :start_date).where(status: "refused")
+  end
+
+  def accept
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "accepted")
+    redirect_to owner_bookings_path
+  end
+
+  def refuse
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "refused")
+    redirect_to owner_bookings_path
   end
 end
